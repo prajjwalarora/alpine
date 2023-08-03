03a21678de42bdeaf5b4b5b2b75a1a1e56948fcc
Prajjwal Arora <aroraprajjwal@gmail.com>
2023-08-03 22:00:29 +0530

    Added more info

--- app.config.ts
+++ app.config.ts
@@ -1,7 +1,7 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'Prajjwal Arora',
+    description: 'Personal Portfolio of Prajjwal Arora',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
@@ -9,30 +9,38 @@ export default defineAppConfig({
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/logo-prajjwal.svg', // path of the logo
+        pathDark: '/logo-prajjwal.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'Prajjwal Arora' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: 'Prajjwal Arora'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'AroraPrajjwal',
+      instagram: 'prajjwal.arora',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'prajjwalarora'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
